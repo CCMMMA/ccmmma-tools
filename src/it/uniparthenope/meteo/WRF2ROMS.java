71cea954619265a3ac6e911381f4bf0da76050e1
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-09-19 07:33:00 +0200

    Refactory Bugfix

--- src/it/uniparthenope/meteo/WRF2ROMS.java
+++ src/it/uniparthenope/meteo/WRF2ROMS.java
@@ -4,7 +4,9 @@
  */
 package it.uniparthenope.meteo;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jncregridder.roms.ROMSGrid;
@@ -23,30 +25,67 @@ public class WRF2ROMS {
     private double[] interpLevels = new double[] {1000.,950.,900.,850.,800.,750.,700.,650.,600.,550.,500.,450.,400.,350.,300.,250.,200.,150.,100};
     
     public static void main(String[] args) throws IOException, NCRegridderException, InvalidRangeException, InterpolatorException {
+
+        /*
+        new WRF2ROMS(
+                "/Users/raffaelemontella/dev/ccmmma/roms/data/Campania_270m.nc",
+                "/Users/raffaelemontella/dev/ccmmma/roms/wrfout/",
+                0,
+                "/Users/raffaelemontella/dev/ccmmma/roms/forcing/wind-d03.nc");
+        */
         try {
             
-            if (args.length != 6) {
+            if (args.length != 4) {
                 System.out.println("Usage:");
-                System.out.println("WRF2ROMS gridFilename wrfFilename timeOffset forcingBulkFilename forcingUVStressFilename csvPath");
+                System.out.println("WRF2ROMS gridFilename wrfFilename|wrfFilespath timeOffset forcingFilename");
                 System.exit(0);
             }
-            new WRF2ROMS(args[1],Integer.parseInt(args[2]), args[0], args[3], args[4],args[5]);
+            new WRF2ROMS(args[0],args[1],Integer.parseInt(args[2]), args[3]);
             
         } catch (InvalidRangeException ex) {
             Logger.getLogger(WRF2ROMS.class.getName()).log(Level.SEVERE, null, ex); 
         } 
-        
+
          
         
             
     }
 
-    public WRF2ROMS(String wrfDataFilename, int wrfTimeOffset, String romsGridFilename, String romsForcingBulkFilename, String romsForcingUVStressFilename, String csvPath) throws IOException, InvalidRangeException, NCRegridderException, InterpolatorException {
-        
-        WRFData wrfData = new WRFData(wrfDataFilename,WRFData.INTERPMETHOD_DEFAULT,interpLevels);
+    public WRF2ROMS(  String romsGridFilename,  String wrfFilenameOrFilesPath, int timeOffset, String romsForcingFilename) throws IOException, InvalidRangeException, NCRegridderException, InterpolatorException {
+
+        // Open the ROMS Grid
         ROMSGrid romsGrid = new ROMSGrid(romsGridFilename);
-        ROMSWind romsWind = new ROMSWind(romsForcingBulkFilename, romsForcingUVStressFilename,romsGrid,wrfData,wrfTimeOffset,csvPath);
-        romsWind.make();
+
+        ROMSWind romsWind=new ROMSWind(romsForcingFilename,romsGrid);;
+
+        File folder = new File(wrfFilenameOrFilesPath);
+        if (folder.isDirectory()) {
+            File[] listOfFiles = folder.listFiles();
+            Arrays.sort(listOfFiles);
+            if (listOfFiles != null) {
+                int count = 0;
+                for (int i = 0; i < listOfFiles.length; i++) {
+                    if (listOfFiles[i].isFile()) {
+                        if (listOfFiles[i].getName().startsWith("wrf")) {
+                            String wrfDataFilename = listOfFiles[i].getPath();
+                            System.out.println("File " + wrfDataFilename);
+                            WRFData wrfData = new WRFData(wrfDataFilename, WRFData.INTERPMETHOD_DEFAULT, interpLevels);
+                            if (count == 0) {
+                                System.out.println("Inizialization.");
+                                romsWind.init(wrfData, timeOffset);
+                            }
+                            System.out.println("Adding.");
+                            romsWind.add(wrfData, timeOffset);
+                            count++;
+                        }
+                    }
+                }
+            }
+        } else {
+            WRFData wrfData = new WRFData(wrfFilenameOrFilesPath, WRFData.INTERPMETHOD_DEFAULT, interpLevels);
+            romsWind.init(wrfData, timeOffset);
+            romsWind.add(wrfData, timeOffset);
+        }
     }
     
    
